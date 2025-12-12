133a42fe97cd246417c4e35acd60bf8eddbe0500
EdgarMisaelOsornio <edgarmisaelosornio@gmail.com>
2025-12-12 13:05:35 -0600

    Add files via upload

--- CLAVES_ETRANSPORTE/js/main.js
+++ CLAVES_ETRANSPORTE/js/main.js
@@ -1,8 +1,8 @@
-import { iniciarBuscador } from "/js/buscador.js";
-import { generar } from "/js/generador.js";
-import { descargarExcel, descargarWord } from "/js/exportaciones.js";
-import { cargarSelectDirecciones, agregarOficinasPorDireccion } from "/js/filtros.js";
-import { listaDirecciones } from "/data/direccionesLista.js";
+import { iniciarBuscador } from "./js/buscador.js";
+import { generar } from "./js/generador.js";
+import { descargarExcel, descargarWord } from "./js/exportaciones.js";
+import { cargarSelectDirecciones, agregarOficinasPorDireccion } from "./js/filtros.js";
+import { listaDirecciones } from "./data/direccionesLista.js";
 
 window.onload = () => {
     iniciarBuscador();
